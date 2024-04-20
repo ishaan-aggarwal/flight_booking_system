70f4b86d2e3152e702f7f5272f237a4e065a331e
Ishaan Aggarwal <154077101+ishaan-aggarwal@users.noreply.github.com>
2024-04-20 14:27:27 +0530

    Add message flashing

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,33 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash, redirect
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return '<p>Login</p>'
+    return render_template('login.html')
 
 @auth.route('/logout')
 def logout():
     return '<p>Logout</p>'
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return '<p>Sign Up</p>'
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if not name or not email or not password1 or not password2:
+            flash('Please fill out all fields.', category='error')
+        elif len(email) < 4:
+            flash('Email must be at least 4 characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 6:
+            flash('Password must be at least 6 characters.', category='error')
+        else:
+            flash('Account created successfully', category='success')
+            pass
+
+    return render_template('sign_up.html')
